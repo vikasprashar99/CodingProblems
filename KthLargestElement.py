fa8fc60bdc33fee8e516c40ac7da7d7a6a76fdf0
Vikas <vikasprashar99@gmail.com>
2021-06-18 21:41:08 +0530

    Create KthLargestElement.py

---
+++ KthLargestElement.py
@@ -0,0 +1,17 @@
+import heapq
+
+def KthLargestElement(arr,k):
+  # creating a min heap for largest element
+    heap=[]
+    for i in range(len(arr)):
+        heapq.heappush(heap,arr[i])
+        if len(heap)>k:
+            heapq.heappop(heap)
+            
+    return heapq.heappop(heap)
+
+arr=[7,10,4,3,20,15]
+k=1
+KthLargestElement(arr,k) 
+
+

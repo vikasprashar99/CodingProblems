bc0f8508b86729b40dc6905ca2ebd48e8366a09b
Vikas <vikasprashar99@gmail.com>
2021-06-22 14:47:10 +0530

    Create BestTimetoBuyandSellStock.py

---
+++ BestTimetoBuyandSellStock.py
@@ -0,0 +1,14 @@
+import math
+
+def BestTimetoBuyandSellStock():
+    prices =[7,1,5,3,6,4]
+    leastsofar=math.inf
+    maxCurrentProfit=0
+    maxProfit=0
+    
+    for i in range(len(prices)):
+        if prices[i]<leastsofar:
+            leastsofar=prices[i]
+        
+        maxCurrentProfit=prices[i]-leastsofar
+        maxProfit=max(maxProfit,maxCurrentProfit)

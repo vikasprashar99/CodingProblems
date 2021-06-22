e683644059bb78788929c17328e68d65dedc44a3
Vikas <vikasprashar99@gmail.com>
2021-06-22 16:41:28 +0530

    Create zeroSumSubarr.py

---
+++ zeroSumSubarr.py
@@ -0,0 +1,15 @@
+# Find if there is any subarray with sum equal to 0
+
+a=[4,2,1,6]
+
+def zeroSumSubarr(a):
+    s=set()
+    temp=0
+    for i in range(len(a)):
+        temp+=a[i]
+        if temp in s or temp==0:
+            return True
+        else:
+            s.add(temp)
+    return False
+zeroSumSubarr(a)

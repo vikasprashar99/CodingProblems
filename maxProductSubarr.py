7052899428496c3a1a7c06e130c13d2577832f04
Vikas <vikasprashar99@gmail.com>
2021-06-23 14:46:46 +0530

    Create maxProductSubarr.py

---
+++ maxProductSubarr.py
@@ -0,0 +1,12 @@
+# Max Product Subarr
+# we take max as well as the min product 
+
+nums=[6, -3, -10, 0, 2]
+prevMin = prevMax = 1
+maxProduct = nums[0]
+for num in nums:
+    temp=prevMin
+    prevMin= min(num,num*prevMin,num*prevMax)
+    prevMax=max(num,num*temp,num*prevMax)
+    maxProduct = max(maxProduct,prevMax)
+print(maxProduct)

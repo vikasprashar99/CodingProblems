6fc8282229241ddc596b686f13d6027dbc1adf77
Vikas <vikasprashar99@gmail.com>
2021-06-24 12:23:55 +0530

    Create unionIntersection.py

---
+++ unionIntersection.py
@@ -0,0 +1,24 @@
+
+
+def unionOfArrays(a,b):
+    s=set()
+    for i in range(len(a)):
+        s.add(a[i])
+    
+    for j in range(len(b)):
+        s.add(b[j])
+    
+    print(len(s))
+def intersectionArrays(a,b):
+    s=set()
+    for i in range(len(a)):
+        s.add(a[i])
+        
+    for i in range(len(b)):
+        if b[i] in s:
+            print(b[i])
+        
+a=[1,2,3,4,5]
+b=[1,2,3]
+unionOfArrays(a,b)  
+intersectionArrays(a,b)

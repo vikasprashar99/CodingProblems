0f900941556e5b2945bb85db5dd32da6ab1e5d19
Vikas <vikasprashar99@gmail.com>
2021-06-28 22:14:38 +0530

    Create MaxConsecutiveOnes.py

---
+++ MaxConsecutiveOnes.py
@@ -0,0 +1,19 @@
+# Max Consecutive Ones III
+
+nums = [1,1,1,0,0,0,1,1,1,1,0]
+k = 2
+
+
+j=0
+count=0
+ans=0
+for i in range(len(nums)):
+    if nums[i]==0:
+        count+=1
+        
+    while count>k:
+        if nums[j]==0:
+            count-=1
+        j+=1
+    
+    ans=max(ans,i-j+1)
